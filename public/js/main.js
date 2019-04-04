65d774976aea0005b655fda013c2b052dd95c391
ABGEO <takalandzet@gmail.com>
2019-04-04 23:50:21 +0400

    Move Toastr settings to main.js.

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Toastr settings.
+ *
+ * @type {{newestOnTop: boolean, debug: boolean, showMethod: string, extendedTimeOut: string, onclick: null, showDuration: string, timeOut: string, hideEasing: string, positionClass: string, hideDuration: string, preventDuplicates: boolean, closeButton: boolean, showEasing: string, progressBar: boolean, hideMethod: string}}
+ */
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
